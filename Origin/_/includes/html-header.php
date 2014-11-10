9ae545e621e7d06490170908497284b1f194c5d1
Dave J. Fox <dave@filamentlab.com>
2014-11-10 16:18:13 +0000

    First commit. Base set up.

---
+++ Origin/_/includes/html-header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html class="no-js" lang="en">
+	<head>
+		<title><?php bloginfo( 'name' ); ?><?php wp_title( '|' ); ?></title>
+		<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	  	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<?php wp_head(); ?>
+	</head>
+	<body <?php body_class(); ?>>
