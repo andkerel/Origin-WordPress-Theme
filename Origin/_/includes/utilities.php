9f3cecae60818f5e491e9401602cb2db173c4349
Dave Fox <djafox@gmail.com>
2017-09-10 10:35:41 +0100

    Slight re-structure.

--- Origin/_/includes/utilities.php
+++ Origin/_/includes/utilities.php
@@ -1,28 +1,28 @@
 <?php
 
 	/**
-	 * Origin_Utilities
-	 *
-	 * Starkers Utilities Class v.1.1
-	 *
-	 * @package 	WordPress
-	 * @subpackage 	Starkers
-	 * @since 		Starkers 4.0
-	 *
-	 * We've included a number of helper functions that we use in every theme we produce.
-	 * The main one that is used in Starkers is Origin_Utilities::add_slug_to_body_class(), this will add the page or post slug to the body class
-	 *
-	 */
+	* Origin_Utilities
+	*
+	* Starkers Utilities Class v.1.1
+	*
+	* @package 	WordPress
+	* @subpackage 	Starkers
+	* @since 		Starkers 4.0
+	*
+	* We've included a number of helper functions that we use in every theme we produce.
+	* The main one that is used in Starkers is Origin_Utilities::add_slug_to_body_class(), this will add the page or post slug to the body class
+	*
+	*/
 	 
 	 class Origin_Utilities {
 
     	/**
-    	 * Print a pre formatted array to the browser - very useful for debugging
-    	 *
-    	 * @param 	array
-    	 * @return 	void
-    	 * @author 	Keir Whitaker
-    	 **/
+    	* Print a pre formatted array to the browser - very useful for debugging
+    	*
+    	* @param 	array
+    	* @return 	void
+    	* @author 	Keir Whitaker
+    	**/
     	public static function print_a( $a ) {
     		print( '<pre>' );
     		print_r( $a );
@@ -30,14 +30,14 @@
     	}
 
     	/**
-    	 * Simple wrapper for native get_template_part()
-    	 * Allows you to pass in an array of parts and output them in your theme
-    	 * e.g. <?php get_template_parts(array('part-1', 'part-2')); ?>
-    	 *
-    	 * @param 	array 
-    	 * @return 	void
-    	 * @author 	Keir Whitaker
-    	 **/
+    	* Simple wrapper for native get_template_part()
+    	* Allows you to pass in an array of parts and output them in your theme
+    	* e.g. <?php get_template_parts(array('part-1', 'part-2')); ?>
+    	*
+    	* @param 	array 
+    	* @return 	void
+    	* @author 	Keir Whitaker
+    	**/
     	public static function get_template_parts( $parts = array() ) {
     		foreach( $parts as $part ) {
     			get_template_part( $part );
@@ -45,13 +45,13 @@
     	}
 
     	/**
-    	 * Pass in a path and get back the page ID
-    	 * e.g. Origin_Utilities::get_page_id_from_path('about/terms-and-conditions');
-    	 *
-    	 * @param 	string 
-    	 * @return 	integer
-    	 * @author 	Keir Whitaker
-    	 **/
+    	* Pass in a path and get back the page ID
+    	* e.g. Origin_Utilities::get_page_id_from_path('about/terms-and-conditions');
+    	*
+    	* @param 	string 
+    	* @return 	integer
+    	* @author 	Keir Whitaker
+    	**/
     	public static function get_page_id_from_path( $path ) {
     		$page = get_page_by_path( $path );
     		if( $page ) {
@@ -62,13 +62,13 @@
     	}
 
     	/**
-    	 * Append page slugs to the body class
-    	 * NB: Requires init via add_filter('body_class', 'add_slug_to_body_class');
-    	 *
-    	 * @param 	array 
-    	 * @return 	array
-    	 * @author 	Keir Whitaker
-    	 */
+    	* Append page slugs to the body class
+    	* NB: Requires init via add_filter('body_class', 'add_slug_to_body_class');
+    	*
+    	* @param 	array 
+    	* @return 	array
+    	* @author 	Keir Whitaker
+    	*/
     	public static function add_slug_to_body_class( $classes ) {
     		global $post;
 	   
@@ -82,12 +82,12 @@
     	}
 	
     	/**
-    	 * Get the category id from a category name
-    	 *
-    	 * @param 	string 
-    	 * @return 	string
-    	 * @author 	Keir Whitaker
-    	 */
+    	* Get the category id from a category name
+    	*
+    	* @param 	string 
+    	* @return 	string
+    	* @author 	Keir Whitaker
+    	*/
     	public static function get_category_id( $cat_name ){
     		$term = get_term_by( 'name', $cat_name, 'category' );
     		return $term->term_id;
